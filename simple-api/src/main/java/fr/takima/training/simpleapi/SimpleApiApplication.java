244a2e9a2be1a8e14b9284fbf4827bda722b2d7e
Moha <mohamedguef@gmail.com>
2025-02-04 14:56:24 +0100

    coquille

--- simple-api/src/main/java/fr/takima/training/simpleapi/SimpleApiApplication.java
+++ simple-api/src/main/java/fr/takima/training/simpleapi/SimpleApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimpleApiApplication {
 
 	public static void main(String[] args) {
+		coquille
 		SpringApplication.run(SimpleApiApplication.class, args);
 	}
 
